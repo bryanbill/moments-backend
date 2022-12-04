cdd8c7d08c228ff654779437bcbee59e76cae568
bryanbill <emobrya@gmail.com>
2022-12-05 00:01:36 +0300

    ft:Route - Adds auth route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,3 +1,8 @@
+import { register } from 'controllers';
 import express from 'express';
+import { protect } from 'middleware/auth';
 const router = express.Router();
 
+router.post('/register', register);
+
+export default router;
