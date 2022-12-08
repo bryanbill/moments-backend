fd844c5c27693aa1bb91807207516699def01323
bryanbill <emobrya@gmail.com>
2022-12-08 10:23:56 +0300

    fix:Types - Extends the request method to have a 'user' property

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,20 @@
+import mongoose from 'mongoose';
+import { UserDoc } from '../src/models/User';
+import { Request } from 'express';
+declare global {
+    namespace Express {
+        interface User extends UserDocument {
+        }
+
+        interface Request {
+
+            user: UserDoc;
+        }
+    }
+}
+
 declare module 'express-serve-static-core' {
-    export interface Request {
-        user: iUser
+    export interface ParamsDictionary {
+        [key: string]: string | number | mongoose.Types.ObjectId;
     }
 }
