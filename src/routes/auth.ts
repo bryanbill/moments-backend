e552ba78370c51c004c4bca74596d3afc419dd4c
bryanbill <emobrya@gmail.com>
2022-12-08 10:23:11 +0300

    ft:Routes  - Adds other auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,16 @@
-import { register } from '../controllers';
+import { forgotPassword, getMe, login, logout, register, resetPassword, updateDetails, updatePassword, uploadChannelAvatar } from '../controllers';
 import express from 'express';
-import { protect } from 'middleware/auth';
+import { protect } from '../middleware/auth';
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
+router.post('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
+router.put('/avatar', protect, uploadChannelAvatar);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 export default router;
